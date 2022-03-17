e05aff797598f3b6cefd0c9c96bf5f86450f4cb6
jcrean <jared@jaredcrean.com>
2022-03-17 17:58:47 -0600

    added some more usefull plugins getting close to where I want to be

---
+++ firefox/user.js
@@ -0,0 +1,78 @@
+///  NATURAL SMOOTH SCROLLING V3 - AveYo, 2020-2021                     preset     [default]
+user_pref("apz.allow_zooming",                                          true);//NSS   [true]
+user_pref("apz.force_disable_desktop_zooming_scrollbars",              false);//NSS  [false]
+user_pref("apz.paint_skipping.enabled",                                 true);//NSS   [true]
+user_pref("apz.windows.use_direct_manipulation",                        true);//NSS   [true]
+user_pref("dom.event.wheel-deltaMode-lines.always-disabled",            true);//NSS  [false]
+user_pref("general.smoothScroll.currentVelocityWeighting",             "1.0");//NSS ["0.25"]
+user_pref("general.smoothScroll.durationToIntervalRatio",               1000);//NSS    [200]
+user_pref("general.smoothScroll.lines.durationMaxMS",                    100);//NSS    [150]
+user_pref("general.smoothScroll.lines.durationMinMS",                      0);//NSS    [150]
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS",               100);//NSS    [200]
+user_pref("general.smoothScroll.mouseWheel.durationMinMS",                 0);//NSS     [50]
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);//NSS    [120]
+user_pref("general.smoothScroll.msdPhysics.enabled",                    true);//NSS  [false]
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   175);//NSS   [1250]
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      1500);//NSS   [1000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           10);//NSS     [12]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     "1.1");//NSS    [1.3]
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     1000);//NSS   [2000]
+user_pref("general.smoothScroll.other.durationMaxMS",                    100);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMinMS",                      0);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMaxMS",                    100);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMinMS",                      0);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMaxMS",                   100);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMinMS",                     0);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMaxMS",               100);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMinMS",                 0);//NSS    [150]
+user_pref("general.smoothScroll.stopDecelerationWeighting",            "1.0");//NSS  ["0.4"]
+user_pref("layers.async-pan-zoom.enabled",                              true);//NSS   [true]
+user_pref("layout.css.scroll-behavior.spring-constant",                "250");//NSS    [250]
+user_pref("mousewheel.acceleration.factor",                               10);//NSS     [10]
+user_pref("mousewheel.acceleration.start",                                -1);//NSS     [-1]
+user_pref("mousewheel.default.delta_multiplier_x",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_y",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_z",                       100);//NSS    [100]
+user_pref("mousewheel.min_line_scroll_amount",                             0);//NSS      [5]
+user_pref("mousewheel.system_scroll_override.enabled",                  true);//NSS   [true]
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false);//NSS   [true]
+user_pref("mousewheel.system_scroll_override.horizontal.factor",         200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override.vertical.factor",           200);//NSS    [200]
+user_pref("mousewheel.transaction.timeout",                             1500);//NSS   [1500]
+user_pref("toolkit.scrollbox.horizontalScrollDistance",                    4);//NSS      [5]
+user_pref("toolkit.scrollbox.verticalScrollDistance",                      3);//NSS      [3]
+///  this preset will reset couple extra variables for consistency
+///  copy into your firefox profile as user.js, add to existing one or edit via about:config
+
+/// AveYo: uncomment if scroll too fast
+//user_pref("mousewheel.system_scroll_override.enabled", false);            
+
+/// AveYo: uncomment if you prefer even more smoothness than V3 
+//user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   125);//NSS+  [1250]
+//user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       500);//NSS+  [1000]
+
+// AveYo: uncomment for something more sharper, then fiddle with these going lower for smoothness:
+//user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",  1250);
+//user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      1000);
+
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);                      // o Sponsored content
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);              // o Sponsored Top Sites
+user_pref("browser.search.suggest.enabled", true);                                         // o Search suggestions master toggle
+
+user_pref("browser.taskbar.lists.enabled", false);                                         // o Windows jumplist recent
+user_pref("browser.taskbar.lists.frequent.enabled", false);                                // o Windows jumplist frequent
+user_pref("browser.taskbar.lists.tasks.enabled", false);                                   // o Windows jumplist tasks
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);                         // o Other Bookmarks on toolbar - long awaited pref
+//user_pref("browser.toolbars.bookmarks.visibility", "always");                            // / Show bookmarks toolbar: newtab / always / never
+user_pref("browser.urlbar.clickSelectsAll", false);                                        // o Select all url on Click
+user_pref("browser.urlbar.ctrlCanonizesURLs", false);                                      // x Ctrl+Enter opens new tab instead of lame canonize
+user_pref("browser.urlbar.decodeURLsOnCopy", true);                                        // x Decode urlencoded link on copy
+user_pref("browser.urlbar.doubleClickSelectsAll", true);                                   // x Select all url on doubleClick
+user_pref("browser.urlbar.formatting.enabled", false);                                     // o Domain highlight that makes full url hard to read
+//user_pref("browser.urlbar.switchTabs.adoptIntoActiveWindow", true);                      // / Search in active tabs pulls tab from all windows
+user_pref("browser.urlbar.trimURLs", false);                                               // o Display all parts of the url in the urlbar
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);                              // x Enable Add button in Search engines options
+user_pref("browser.xul.error_pages.expert_bad_cert", true);                                // x Display advanced info on Insecure Connection
+//user_pref("browser.zoom.siteSpecific", false);                                           // / Disable remembering zoom per site
+//user_pref("dom.confirm_repost.testing.always_accept", true);                             // / Hide confirm dialog when reloading a POST request
+user_pref("dom.disable_beforeunload", true);                                               // x Confirm you want to leave dialog on page close
---
+++ zsh/zshrc
@@ -0,0 +1,90 @@
+# initialize direnv
+eval $(thefuck --alias)
+(( ${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+#POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+
+
+# Path to your oh-my-zsh installation.
+export ZSH="/home/jcrean/.oh-my-zsh"
+
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+#ZSH_THEME="agnoster"
+#ZSH_THEME="random"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# Uncomment the following line to enable command auto-correction.
+#ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# Caution: this setting can cause issues with multiline prompts (zsh 5.7.1 and newer seem to work)
+# See https://github.com/ohmyzsh/ohmyzsh/issues/5765
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+HIST_STAMPS="mm.dd.yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(fzf ufw git git-extras aws ssh-agent aws-mfa docker fzf-tab zsh-syntax-highlighting colorize colored-man-pages terraform git-extras)
+
+source $ZSH/oh-my-zsh.sh
+
+
+# User configuration
+
+source $ZSH_CUSTOM/aliases.zsh
+[ -f $ZSH_CUSTOM/kubectl_aliases.zsh ] && source $ZSH_CUSTOM/kubectl_aliases.zsh
+
+#export TERM=screen-256color
+
+#export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+# export the qt theme
+#export QT_QPA_PLATFORMTHEME=gtk2
+# Autoload zsh zsh-add-hook
+autoload -U add-zsh-hook
+
+autoload -U compinit && compinit //override comp
+
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+# disable sort when completing `git checkout`
+#zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
+
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ /usr/bin/kubectl ]] && source <(kubectl completion zsh)
+
+# Created by `pipx` on 2022-03-04 07:36:39
+export PATH="$PATH:/home/jcrean/.local/bin:/snap/bin"
