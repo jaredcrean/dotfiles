0fe30fc7e4c3ff8ec3e308c5362274ef1e258c61
jcrean <jared@jaredcrean.com>
2022-03-17 20:21:55 -0600

    updated to use stow

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-## Move old dotfile to bakup dir ##
-
-FILES = "zshrc vimrc bashrc tmux"
-OLDDIR = ~/old_dotfiles
-NEWDIR = "~/dotfiles"
-
-
-mkdir -p ~/$OLDDIR
-mkdir -p ~/$DOTFILES
-
-cd $NEWDIR
-
-for file in $FILES; do
-    echo "Move old files to $OLDDIR"
-    mv ~/.$file ~/$OLDDIR
-    echo "Link new files to $NEWDIR/$file"
-    ln -sf $NEWDIR/$files ~/.$file
-done
-
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/touchegg.conf ~/.config/touchegg/touchegg.conf
-
+stow --verbose --target=$HOME tmux
+stow --verbose --target=$HOME neovim
+stow --verbose --target=$HOME zsh
+stow --verbose --target=$HOME bash
+#stow --verbose --target=$HOME i3
