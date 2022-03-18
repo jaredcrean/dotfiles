6e86b975f9c50f79daa8ba54c3d0deb17b37e0d2
jcrean <jared@jaredcrean.com>
2022-03-18 14:50:40 -0600

    fixed gitignore file

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,32 @@
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+-- ━━━━━━━━━━━━━❰ Load/Source Configs ❱━━━━━━━━━━━━━ --
+
+-- plugin config to improve start-up time.
+-- it should be always on top on init.lua file
+--require('jc.impatient') -- impatient needs to be setup before any other lua plugin is loaded so it is recommended you add the following near the start of your
+--require('jc.filetype_nvim') -- Easily speed up your neovim startup time!
+
+require "jc.configs" -- plugin independent configs
+require "jc.mappings" -- plugin independent mappings
+require "jc.plugins"
+require "jc.lsp"
+require "jc.cmp" -- plugin independent mappings
+require "jc.project"
+require "jc.telescope"
+require "jc.gitsigns"
+require "jc.themes.material"
+require "jc.lualine"
+require "jc.treesitter"
+require "jc.which-key"
+require "jc.nvim-tree"
+require "jc.nvim-web-devicons"
+require "jc.autopairs"
+require "jc.bufferline"
+require "jc.alpha"
+-- require "jc.feline"
+require "jc.nvim-comment"
+require "jc.toggleterm"
+require "jc.auto-session"
+require "jc.session-lens"
+require "jc.lualine"
+
