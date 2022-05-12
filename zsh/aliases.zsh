e6ffb4c66104fe0e963afacf6a985dc1e694fef7
jcrean <jared@jaredcrean.com>
2022-05-12 11:31:20 -0600

    Updates to theme and mappings

---
+++ zsh/aliases.zsh
@@ -0,0 +1,46 @@
+alias reload="source ~/.zshrc"
+alias cdw="cd $HOME/gitrepos/Projects/work/northlabs/"
+alias cdr="cd $HOME/gitrepos/"
+alias cdd="cd $HOME/.dotfiles"
+
+alias vim="nvim"
+alias htop="btop"
+#alias rm='rm -I'
+#alias mv='mv -I'
+alias cp='cp -i'
+# AWS set profile
+#alias asp="asp"
+# AWS get profile
+alias agp="agp"
+alias cdnas="cd /nfs/nas/"
+# Make clip put selection in clipboard rather then middle mouse click
+alias xclip="xclip -selection clipboard"
+alias reload="omz reload"
+#alias icat="kitty +kitten icat"
+alias cat="bat"
+
+
+## Colors
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+alias ip='ip -color=auto'
+alias yay='paru'
+
+# Better ls
+alias ls='exa --icons --long --git'
+alias lsa='exa -a --icons --long --git'
+
+#Flatpak alias to make launching them a little easier
+alias joplin='/var/lib/flatpak/exports/bin/net.cozic.joplin_desktop'
+
+# List the directories in tree format and take an argument for the depth level
+function lst() {
+   if [[ $# -gt 0 ]]
+   then
+     exa --icons --tree --level=$1 --long
+   elif [[ $# -eq 0 ]]
+   then
+     exa --icons --tree --level=1 --long
+     echo "### Add a number to show deeper directories example: lst 3"
+   fi
+}
