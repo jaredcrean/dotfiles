b42687f3c5394624de7f1e228d7bdd34084a9f61
jcrean <jared@jaredcrean.com>
2022-05-23 21:07:32 -0600

    adding wezterm with tmux bindings

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -5,4 +5,5 @@ stow --verbose --target="$HOME" neovim
 stow --verbose --target="$HOME" zsh
 stow --verbose --target="$HOME" bash
 stow --verbose --target="$HOME" i3
+stow --verbose --target="$HOME" wezterm
 #stow --verbose --target=$HOME i3
