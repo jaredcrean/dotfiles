3b6e7140be876664f150c8677e285705841089ff
p3181634 <c-jared.crean@charter.com>
2023-04-11 12:46:06 -0600

    fixed nested function error

--- neovim/.config/nvim/lua/jc/auto-session.lua
+++ neovim/.config/nvim/lua/jc/auto-session.lua
@@ -1,11 +1,38 @@
-require('auto-session').setup {
-    auto_session_suppress_dirs = {'~/', '~/Projects'},
-    log_level = 'error',
-    auto_session_enable_last_session = true,
-    auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
-    auto_session_enabled = true,
-    auto_save_enabled = true,
-    auto_restore_enabled = true,
-    -- the configs below are lua only
-    bypass_session_save_file_types = nil
- }
+-- require('auto-session').setup {
+--     auto_session_suppress_dirs = {'~/', '~/Projects'},
+--     log_level = 'error',
+--     auto_session_enable_last_session = true,
+--     auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+--     auto_session_enabled = true,
+--     auto_save_enabled = true,
+--     auto_restore_enabled = true,
+--     -- the configs below are lua only
+--     bypass_session_save_file_types = nil
+--  }
+
+-- Fix for neotree issues
+local function close_neo_tree()
+  require 'neo-tree.sources.manager'.close_all()
+  vim.notify('closed all')
+end
+
+local function open_neo_tree()
+  vim.notify('opening neotree')
+  require 'neo-tree.sources.manager'.show('filesystem')
+end
+
+require 'auto-session'.setup {
+  auto_session_create_enabled = false,
+  --auto_session_enable_last_session = true,
+  auto_save_enabled = true,
+  auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+  auto_restore_enabled = true,
+  auto_session_use_git_branch = true,
+  bypass_session_save_file_types = { "neo-tree" },
+  pre_save_cmds = {
+    close_neo_tree,
+  },
+  post_restore_cmds = {
+    open_neo_tree,
+  }
+}
